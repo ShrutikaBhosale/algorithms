7bbd3d81eab705a1498d389a6d742ccb04e43d13
ShrutikaBhosale <141473148+ShrutikaBhosale@users.noreply.github.com>
2025-03-11 05:16:04 +0530

    Update SHA256.js

--- SHA256.js
+++ SHA256.js
@@ -73,9 +73,27 @@ const msgExpansion = (msg) => {
   return msg;
 };
 
+const roundConstant = () => {
+  const primes = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
+    157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233,
+    239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
+  ];
+
+  const roundCnst = primes.map((prime) => {
+    const num = Math.cbrt(prime);
+    const fractionalPart = num - Math.floor(num);
+    const a = Math.floor(fractionalPart * Math.pow(2, 32));
+    return a.toString(16);
+  });
+  console.log(roundCnst);
+};
+
 export const main = (text) => {
   const finalRepresentation = messagePadding(text);
   return msgExpansion(msgDecomposition(finalRepresentation));
+  // return computeInitialHash();
 };
 
-console.log(main("abc")); //last step remaining
+console.log(main("abc"));
